6af9adb5b9c406a9d94c86026f52f236ab63a3d0
gonzalorportfolio <gromeroeducation@gmail.com>
2025-10-29 09:55:47 -0400

    init

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # array-hom-practice-f25
+
+## `.find()`
+
+You’re given an array of students. Use .find() to get the student whose score is greater than 90.
+
+```js
+const students = [
+  { name: "Alice", score: 88 },
+  { name: "Brian", score: 95 },
+  { name: "Carla", score: 78 },
+];
+
+// Expected output: { name: "Brian", score: 95 }
+```
+
+## `.findIndex()`
+
+You’re given an array of fruit names. Use .findIndex() to find the index of "mango".
+
+```js
+`const fruits = ["apple", "banana", "mango", "orange"];
+
+// Your code here
+// Expected output: 2
+```
---
+++ index.js
@@ -0,0 +1,32 @@
+// You’re given an array of students. Use .find() to get the student whose score is greater than 90.
+const students = [
+  { name: "Alice", score: 88 },
+  { name: "Brian", score: 95 },
+  { name: "Carla", score: 78 },
+];
+
+// Your code here
+
+
+// Expected output: { name: "Brian", score: 95 }
+
+// You’re given an array of fruit names. Use .findIndex() to find the index of "mango".
+
+const fruits = ["apple", "banana", "mango", "orange"];
+
+// Your code here
+
+
+// Expected output: 2
+
+
+// You’re given an array of objects representing books. Use .map() to create a new array that contains only the titles of the books.
+
+const books = [
+  { title: "The Hobbit", author: "J.R.R. Tolkien" },
+  { title: "1984", author: "George Orwell" },
+  { title: "Pride and Prejudice", author: "Jane Austen" },
+];
+
+// Your code here
+// Expected output: ["The Hobbit", "1984", "Pride and Prejudice"]
